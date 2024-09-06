c88a49747a6e95f48505638c8316e43ffaf2efcc
João Roberto Fernandes Magalhães <120526440+joaorbrto@users.noreply.github.com>
2024-09-06 08:40:50 -0300

    Initial Commit

---
+++ Vask/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Vask
+//
+//  Created by Joao Roberto Fernandes Magalhaes on 06/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Vask/VaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  VaskApp.swift
+//  Vask
+//
+//  Created by Joao Roberto Fernandes Magalhaes on 06/09/24.
+//
+
+import SwiftUI
+
+@main
+struct VaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
