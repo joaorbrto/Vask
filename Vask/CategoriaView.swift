22c428e2469ec787fe2d00a2641b6c1f9c8e731e
João Roberto Fernandes Magalhães <120526440+joaorbrto@users.noreply.github.com>
2024-09-06 09:01:49 -0300

    Classes feitas

---
+++ Vask/CategoriaView.swift
@@ -0,0 +1,18 @@
+//
+//  CategoriaView.swift
+//  Vask
+//
+//  Created by Joao Roberto Fernandes Magalhaes on 06/09/24.
+//
+
+import SwiftUI
+
+struct CategoriaView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoriaView()
+}
---
+++ Vask/Classes.swift
@@ -0,0 +1,8 @@
+//
+//  Classes.swift
+//  Vask
+//
+//  Created by Joao Roberto Fernandes Magalhaes on 06/09/24.
+//
+
+import Foundation
