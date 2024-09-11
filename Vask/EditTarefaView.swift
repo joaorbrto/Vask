00ef00a7a4d510cab8b145b7ba0698776a97a002
João Roberto Fernandes Magalhães <120526440+joaorbrto@users.noreply.github.com>
2024-09-10 21:20:11 -0300

    tarefaview, addview e editview criadas, porem o codigo ta crashando

---
+++ Vask/EditTarefaView.swift
@@ -0,0 +1,18 @@
+//
+//  EditTarefaView.swift
+//  Vask
+//
+//  Created by Joao Roberto Fernandes Magalhaes on 10/09/24.
+//
+
+import SwiftUI
+
+struct EditTarefaView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditTarefaView()
+}
